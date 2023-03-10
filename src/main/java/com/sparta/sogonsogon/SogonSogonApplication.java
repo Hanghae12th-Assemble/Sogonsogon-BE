edeb7d2af63abc919bca93ddfcd9317f640538fa
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-03-10 19:52:45 +0900

    feat:기본 setting

---
+++ src/main/java/com/sparta/sogonsogon/SogonSogonApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sogonsogon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogonSogonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SogonSogonApplication.class, args);
+    }
+
+}
