f48013a9d47d087c11dfe0b60f8cbc420d175a0f
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-03-12 05:04:47 +0900

    feat: MemberController 생성

---
+++ src/main/java/com/sparta/sogonsogon/member/controller/MemberController.java
@@ -0,0 +1,38 @@
+package com.sparta.sogonsogon.member.controller;
+
+import com.sparta.sogonsogon.dto.StatusResponseDto;
+import com.sparta.sogonsogon.member.dto.LoginRequestDto;
+import com.sparta.sogonsogon.member.dto.MemberResponseDto;
+import com.sparta.sogonsogon.member.dto.SignUpRequestDto;
+import com.sparta.sogonsogon.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    //회원 가입
+    @PostMapping("/signup")
+    public StatusResponseDto<String> signup(@RequestBody @Valid SignUpRequestDto requestDto) throws IllegalAccessException {
+        return memberService.signup(requestDto);
+    }
+
+    //로그인
+    @PostMapping("/login")
+    public StatusResponseDto<MemberResponseDto> login(@RequestBody LoginRequestDto requestDto, @Parameter(hidden = true) HttpServletResponse response) throws IllegalAccessException {
+        return memberService.login(requestDto, response);
+    }
+
+
+}
---
+++ src/main/java/com/sparta/sogonsogon/member/dto/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.sogonsogon.member.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Getter
+public class LoginRequestDto {
+    @Email
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+}
