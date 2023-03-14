45c75ab2d9a5e48ae906fbbc77accf5dc1e4c8df
regular_kim <regular.jk.kim@gmail.com>
2023-03-14 23:16:29 +0900

    Update validation rules for SignupRequestDto fields

--- src/main/java/com/sparta/sogonsogon/member/dto/SignUpRequestDto.java
+++ src/main/java/com/sparta/sogonsogon/member/dto/SignUpRequestDto.java
@@ -2,19 +2,22 @@ package com.sparta.sogonsogon.member.dto;
 
 import lombok.Getter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Getter
 public class SignUpRequestDto {
 
-    @Pattern(regexp = "(?=.*?[a-z])(?=.*?[\\d]).{4,10}")
+    // 알파벳 소문자가 1글자 이상, 숫자가 1개 이상 들어가야하는 정규식
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d).+$", message = "알파벳 소문자와 숫자가 포함되어야 합니다.")
+    @Size(min = 8, max = 13, message = "아이디 길이는 4자 이상, 10자 이하입니다.")
     private String membername;
-    @Pattern(regexp = "(?=.*?[a-zA-Z])(?=.*?[\\d])(?=.*?[~!@#$%^&*()_+=\\-`]).{8,15}")
+
+    // 알파벳 소문자와 대문자 최소 1개 이상, 숫자 1개 이상, 특수문자 1개 이상이 들어가야하는 정규식
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[~!@#$%^&*()_+]).+$", message = "알파벳 소문자와 대문자, 특수문자가 포함되어야 합니다.")
+    @Size(min = 8, max = 15, message = "비밀번호 길이는 8자 이상, 15자 이하입니다.")
     private String password;
     @Email
     private String email;
-    @NotNull
+    @NotBlank //  null이 아니고, 최소한 한 개 이상의 공백이 아닌 문자가 포함
     private String nickname;
 }
