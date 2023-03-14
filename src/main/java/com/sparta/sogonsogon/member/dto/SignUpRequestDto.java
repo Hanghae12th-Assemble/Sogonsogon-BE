e4b0e25c6ae95aa402e30bd4931a6e809585c8e2
regular_kim <regular.jk.kim@gmail.com>
2023-03-15 02:36:53 +0900

    add : comments to SignupRequestDto

--- src/main/java/com/sparta/sogonsogon/member/dto/SignUpRequestDto.java
+++ src/main/java/com/sparta/sogonsogon/member/dto/SignUpRequestDto.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.*;
 @Getter
 public class SignUpRequestDto {
 
+    // 400 BAD_REQUEST
+
     // 알파벳 소문자가 1글자 이상, 숫자가 1개 이상 들어가야하는 정규식
     @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d).+$", message = "알파벳 소문자와 숫자가 포함되어야 합니다.")
     @Size(min = 4, max = 10, message = "아이디 길이는 4자 이상, 10자 이하입니다.")
