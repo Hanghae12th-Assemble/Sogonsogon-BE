1f40d7facee22da8b34a6167baaf01727c715f5f
regular_kim <regular.jk.kim@gmail.com>
2023-03-15 03:26:55 +0900

    add : comments

--- src/main/java/com/sparta/sogonsogon/member/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/sogonsogon/member/dto/LoginRequestDto.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 public class LoginRequestDto {
     @Email
-    @NotNull(message = "이메일을 입력해주세요.")
+    @NotNull(message = "이메일을 입력해주세요.") // BAD_REQUEST
     private String email;
     @NotNull(message = "비밀번호 길이는 8자 이상, 15자 이하입니다.")
     private String password;
