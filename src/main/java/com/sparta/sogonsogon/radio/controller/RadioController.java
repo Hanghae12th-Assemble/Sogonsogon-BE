06b41c289febb358193719d5344a9935593a0acc
regular_kim <regular.jk.kim@gmail.com>
2023-03-16 08:14:13 +0900

    feat : enterRadio

--- src/main/java/com/sparta/sogonsogon/radio/controller/RadioController.java
+++ src/main/java/com/sparta/sogonsogon/radio/controller/RadioController.java
@@ -3,12 +3,14 @@ package com.sparta.sogonsogon.radio.controller;
 import com.sparta.sogonsogon.dto.StatusResponseDto;
 import com.sparta.sogonsogon.radio.dto.RadioRequestDto;
 import com.sparta.sogonsogon.radio.dto.RadioResponseDto;
+import com.sparta.sogonsogon.radio.entity.EnterMemberResponseDto;
 import com.sparta.sogonsogon.radio.repository.RadioRepository;
 import com.sparta.sogonsogon.radio.service.RadioService;
 import com.sparta.sogonsogon.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.h2.engine.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -26,39 +28,41 @@ public class RadioController {
     private final RadioRepository radioRepository;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "라디오  생성", description ="라디오 생성" )
+    @Operation(summary = "라디오  생성", description = "라디오 생성")
     public StatusResponseDto<RadioResponseDto> createRadio(@RequestBody @Valid @ModelAttribute RadioRequestDto requestDto,
                                                            @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
-        return StatusResponseDto.success(HttpStatus.CREATED, radioService.createRadio(requestDto,userDetails));
+        return StatusResponseDto.success(HttpStatus.CREATED, radioService.createRadio(requestDto, userDetails));
     }
 
     @GetMapping("/")
-    @Operation(summary = "전체 라디오 조회", description ="전체 라디오 조회" )
-    public StatusResponseDto<List<RadioResponseDto>> getRadios(){
+    @Operation(summary = "전체 라디오 조회", description = "전체 라디오 조회")
+    public StatusResponseDto<List<RadioResponseDto>> getRadios() {
         return StatusResponseDto.success(HttpStatus.OK, radioService.findAllRadios());
     }
 
     @GetMapping("/{radioId}")
-    @Operation(summary = "선택된 라디오 조회", description ="선택된 라디오 조회" )
-    public StatusResponseDto<RadioResponseDto> getRadio(@PathVariable Long radioId){
+    @Operation(summary = "선택된 라디오 조회", description = "선택된 라디오 조회")
+    public StatusResponseDto<RadioResponseDto> getRadio(@PathVariable Long radioId) {
         return StatusResponseDto.success(HttpStatus.OK, radioService.findRadio(radioId));
     }
 
     @PutMapping("/{radioId}")
-    @Operation(summary = "선택된 라디오 정보 수정", description ="선택된 라디오 정보 수정" )
+    @Operation(summary = "선택된 라디오 정보 수정", description = "선택된 라디오 정보 수정")
     public StatusResponseDto<RadioResponseDto> updateRadio(@PathVariable Long radioId,
-                                                            @Valid @ModelAttribute RadioRequestDto requestDto,
-                                                            @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
-        return StatusResponseDto.success(HttpStatus.OK, radioService.updateRadio(radioId,requestDto,userDetails));
+                                                           @Valid @ModelAttribute RadioRequestDto requestDto,
+                                                           @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        return StatusResponseDto.success(HttpStatus.OK, radioService.updateRadio(radioId, requestDto, userDetails));
     }
 
 
     @DeleteMapping("/{radioId}")
-    @Operation(summary = "선택된 라디오 삭제", description ="선택된 라디오 삭제" )
+    @Operation(summary = "선택된 라디오 삭제", description = "선택된 라디오 삭제")
     public StatusResponseDto<RadioResponseDto> deleteRadio(@PathVariable Long radioId,
-                                                            @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails){
-        return radioService.deleteRadio(radioId,userDetails.getUser());
+                                                           @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        radioService.deleteRadio(radioId, userDetails.getUser());
+        return StatusResponseDto.success(HttpStatus.OK, null);
     }
+
     /*
     TODOS
     1. 방참여
@@ -67,4 +71,10 @@ public class RadioController {
         3-1. 아이디로조회
         3-2. 이름으로조회
      */
+    @PostMapping("/enter/{radioId}")
+    @Operation(summary = "선택한 라디오 참여", description = "선택한 라디오 참여")
+    public StatusResponseDto<EnterMemberResponseDto> enterRadio(@PathVariable Long radioId,
+                                                                @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return StatusResponseDto.success(HttpStatus.OK, radioService.enterRadio(radioId, userDetails));
+    }
 }
