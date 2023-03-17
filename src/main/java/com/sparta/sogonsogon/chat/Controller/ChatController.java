1b069ad3db2823f1fa3156e94b5eb95360be347a
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-03-18 02:21:20 +0900

    feat: web socket과 redis 내용 정리

--- src/main/java/com/sparta/sogonsogon/chat/Controller/ChatController.java
+++ src/main/java/com/sparta/sogonsogon/chat/Controller/ChatController.java
@@ -2,6 +2,8 @@ package com.sparta.sogonsogon.chat.Controller;
 
 
 import com.sparta.sogonsogon.chat.Model.Message;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class ChatController {
 //todo : refactoring
+//    private final
 
     @MessageMapping("/hello")
     @SendTo("/topic/roomId")
--- src/main/java/com/sparta/sogonsogon/rtc/config/RedisConfig.java
+++ src/main/java/com/sparta/sogonsogon/rtc/config/RedisConfig.java
@@ -8,12 +8,13 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableRedisRepositories
-public class RedisRepositoryConfig {
+public class RedisConfig {
 
     private final RedisProperties redisProperties;
 
@@ -27,6 +28,7 @@ public class RedisRepositoryConfig {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
         return redisTemplate;
     }
 
