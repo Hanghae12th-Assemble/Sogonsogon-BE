3613cb3551e671d0137ae1b48e185d26be20b1ed
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-03-21 00:16:17 +0900

    feat: MemberSessionDto 생성

---
+++ src/main/java/com/sparta/sogonsogon/member/oauth/dto/MemberSessionDto.java
@@ -0,0 +1,31 @@
+package com.sparta.sogonsogon.member.oauth.dto;
+
+import com.sparta.sogonsogon.member.entity.Member;
+import com.sparta.sogonsogon.member.entity.MemberRoleEnum;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class MemberSessionDto implements Serializable {
+
+    private Long id;
+    private String membername;
+    private String nickname;
+    private String email;
+    private MemberRoleEnum role;
+    private String modifiedAt;
+
+    //Entity -> DTO
+    public MemberSessionDto(Member member){
+        this.id = member.getId();
+        this.membername = member.getMembername();
+        this.nickname = member.getNickname();
+        this.email = member.getEmail();
+        this.role = member.getRole();
+        this.modifiedAt = member.getModifiedAt().toString();
+    }
+
+
+
+}
