7712e373425d3b167ecab0c35224c0d6c9f44b73
regular_kim <regular.jk.kim@gmail.com>
2023-03-21 14:05:42 +0900

    add : radioDtos category fields

--- src/main/java/com/sparta/sogonsogon/radio/dto/RadioRequestDto.java
+++ src/main/java/com/sparta/sogonsogon/radio/dto/RadioRequestDto.java
@@ -1,6 +1,7 @@
 package com.sparta.sogonsogon.radio.dto;
 
 
+import com.sparta.sogonsogon.enums.CategoryType;
 import com.sparta.sogonsogon.radio.entity.Radio;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,8 @@ public class RadioRequestDto {
     private String introduction; // 라디오 관련 간단한 설명
 
     private MultipartFile backgroundImageUrl; //배경화면
+
+    private CategoryType categoryType; // 카테고리
 
 
 }
