13394752a5c3d678fc81a23e0f748a5a9555fb15
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-03-23 00:13:14 +0900

    docs: oauth2user 관련 builder 추가

--- src/main/java/com/sparta/sogonsogon/member/oauth/dto/MemberSessionDto.java
+++ src/main/java/com/sparta/sogonsogon/member/oauth/dto/MemberSessionDto.java
@@ -1,15 +1,19 @@
 package com.sparta.sogonsogon.member.oauth.dto;
 
+import com.mysql.cj.conf.StringProperty;
 import com.sparta.sogonsogon.member.entity.Member;
 import com.sparta.sogonsogon.member.entity.MemberRoleEnum;
+import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 public class MemberSessionDto implements Serializable {
 
-    private Long id;
+//    private Long id;
     private String membername;
     private String nickname;
     private String email;
@@ -19,7 +23,7 @@ public class MemberSessionDto implements Serializable {
 
     //Entity -> DTO
     public MemberSessionDto(Member member){
-        this.id = member.getId();
+//        this.id = member.getId();
         this.membername = member.getMembername();
         this.nickname = member.getNickname();
         this.email = member.getEmail();
@@ -28,6 +32,15 @@ public class MemberSessionDto implements Serializable {
         this.modifiedAt = member.getModifiedAt().toString();
     }
 
+    @Builder
+    public MemberSessionDto(String membername, String nickname, String email, String profileImageUrl){
+        this.membername = membername;
+        this.nickname = nickname;
+        this.email = email;
+        this.role = MemberRoleEnum.SOCIAL;
+        this.profileImageUrl = profileImageUrl;
+        this.modifiedAt = LocalDateTime.now().toString();
+    }
 
 
 }
