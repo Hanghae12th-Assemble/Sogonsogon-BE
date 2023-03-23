5af0ef8b8ade7334b6539eb394823f179f37910b
regular_kim <regular.jk.kim@gmail.com>
2023-03-23 17:22:49 +0900

    style : reformat follow controll

--- src/main/java/com/sparta/sogonsogon/follow/controller/FollowController.java
+++ src/main/java/com/sparta/sogonsogon/follow/controller/FollowController.java
@@ -30,25 +30,6 @@ public class FollowController {
     }
 
 
-//    @PostMapping("/{followerUsername}")
-//    @Operation(summary = "팔로우 추가", description ="팔로우 추가" )
-//    public StatusResponseDto<FollowResponseDto> createFollow(@PathVariable String followerUsername,
-//                                                             @RequestBody FollowRequestDto requestDto,
-//                                                             @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails)throws AccessDeniedException {
-//
-////        followService.createFollow(followerUsername,requestDto,userDetails.getUsername());
-//        return StatusResponseDto.success(HttpStatus.OK ,(FollowResponseDto) followService.createFollow(followerUsername,requestDto,userDetails.getUser()));
-//    }
-//
-//
-//    @DeleteMapping("/{followerUsername}")
-//    @Operation(summary = "팔로우 취소", description ="팔로우 쉬소" )
-//    public StatusResponseDto<String> unFollow(@PathVariable String followerUsername,
-//                                                  @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails)throws AccessDeniedException {
-//        followService.unfollow(followerUsername,userDetails.getUser());
-//        return StatusResponseDto.success(HttpStatus.OK, "팔로우 취소 되었습니다.");
-//    }
-
     @GetMapping("/{memberId}/following")
     @Operation(summary = "유저를 팔로잉하는 모든 사용자 가져오기", description ="유저를 팔로잉하는 모든 사용자 가져오기" )
     public StatusResponseDto<List<FollowResponseDto>> getFollowings(@PathVariable Long memberId){
