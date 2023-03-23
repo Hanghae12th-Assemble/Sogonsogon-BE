6c9a33549171d34267cb34d90ad6481e0e42a24f
regular_kim <regular.jk.kim@gmail.com>
2023-03-23 21:14:23 +0900

    style : comments removed

--- src/main/java/com/sparta/sogonsogon/radio/controller/RadioController.java
+++ src/main/java/com/sparta/sogonsogon/radio/controller/RadioController.java
@@ -5,15 +5,11 @@ import com.sparta.sogonsogon.enums.CategoryType;
 import com.sparta.sogonsogon.radio.dto.RadioRequestDto;
 import com.sparta.sogonsogon.radio.dto.RadioResponseDto;
 import com.sparta.sogonsogon.radio.entity.EnterMemberResponseDto;
-import com.sparta.sogonsogon.radio.repository.RadioRepository;
 import com.sparta.sogonsogon.radio.service.RadioService;
 import com.sparta.sogonsogon.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
-import org.h2.engine.User;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestController
@@ -43,22 +38,6 @@ public class RadioController {
     public StatusResponseDto<List<RadioResponseDto>> getRadios() {
         return StatusResponseDto.success(HttpStatus.OK, radioService.findAllRadios());
     }
-
-//
-//    @GetMapping("/{radioId}")
-//    @Operation(summary = "선택된 라디오 조회", description ="선택된 라디오 조회" )
-//    public StatusResponseDto<RadioResponseDto> getRadio(@PathVariable Long radioId){
-//        return StatusResponseDto.success(HttpStatus.OK, radioService.findRadio(radioId));
-//    }
-//
-//    @PutMapping(value = "/{radioId}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    @Operation(summary = "선택된 라디오 정보 수정", description ="선택된 라디오 정보 수정" )
-//    public StatusResponseDto<RadioResponseDto> updateRadio(@PathVariable Long radioId,
-//                                                           @Valid @ModelAttribute RadioRequestDto requestDto,
-//                                                           @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
-//        return StatusResponseDto.success(HttpStatus.OK, radioService.updateRadio(radioId,requestDto,userDetails));
-//    }
-
 
     @DeleteMapping("/{radioId}")
     @Operation(summary = "선택된 라디오 삭제", description = "선택된 라디오 삭제")
--- src/main/java/com/sparta/sogonsogon/radio/service/RadioService.java
+++ src/main/java/com/sparta/sogonsogon/radio/service/RadioService.java
@@ -1,6 +1,5 @@
 package com.sparta.sogonsogon.radio.service;
 
-import com.sparta.sogonsogon.dto.StatusResponseDto;
 import com.sparta.sogonsogon.enums.CategoryType;
 import com.sparta.sogonsogon.enums.ErrorMessage;
 import com.sparta.sogonsogon.jwt.JwtUtil;
@@ -21,17 +20,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 
 @Service
@@ -53,18 +49,10 @@ public class RadioService {
                 () -> new InsufficientAuthenticationException(ErrorMessage.ACCESS_DENIED.getMessage()) // 401 Unauthorized
         );
         // 라디오 방 이름 중복여보 확인
-//        if (radioRepository.findByTitle(requestDto.getTitle()) != null) {
-//            throw new IllegalArgumentException("이미 존재하는 라디오 제목입니다.");
-//        }
         Optional<Radio> found = radioRepository.findByTitle(requestDto.getTitle());
         if (found.isPresent()) {
             throw new DuplicateKeyException(ErrorMessage.DUPLICATE_RADIO_NAME.getMessage()); // 409 Conflict
         }
-
-//        if (requestDto.getTitle() == null) {
-//            throw new IllegalArgumentException("제목을 입력해주세요"); // 400 Bad Request
-//        }
-//       해당 내용은 requestDto 에서 검증할 내용이기 때문에 제거
 
         // 라디오 룸 배경이미지 추가
         String imageUrl = s3Uploader.uploadFiles(requestDto.getBackgroundImageUrl(), "radioImages");
