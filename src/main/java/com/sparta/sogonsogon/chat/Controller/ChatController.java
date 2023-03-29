71ffbad3115e9791736d0f7fdba998091cc94aeb
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-03-29 19:45:49 +0900

    docs: ChatController내부 코드 수정

--- src/main/java/com/sparta/sogonsogon/chat/Controller/ChatController.java
+++ src/main/java/com/sparta/sogonsogon/chat/Controller/ChatController.java
@@ -1,11 +1,15 @@
 package com.sparta.sogonsogon.chat.Controller;
 
 
-import com.sparta.sogonsogon.chat.Model.Message;
+import com.sparta.sogonsogon.chat.dto.ChattingDto;
+import com.sparta.sogonsogon.chat.service.ChatService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 //todo : refactoring
 //    private final
+    private final ChatService chatService;
+    private final SimpMessagingTemplate template;
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/roomId")
-    public Message boradCast(Message message){
-        return message;
+    @MessageMapping("/{radioId}")
+    @SendTo("/chat/{radioId}")
+    @ApiOperation(value = "chatting" ,  notes = "라디오 채팅 기능")
+    public void createChat(@DestinationVariable Long radioId, ChattingDto chattingDto){
+        chatService.createChat(radioId, chattingDto);
+        template.convertAndSend("/chat/" + radioId, chattingDto);
     }
 }
