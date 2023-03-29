24a178dbd1bf5ef8c1a8f61bc6980c82a39e8dfa
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-03-29 19:47:22 +0900

    docs: Message.java -> Chat.java 이름변경

--- src/main/java/com/sparta/sogonsogon/chat/entity/Chat.java
+++ src/main/java/com/sparta/sogonsogon/chat/entity/Chat.java
@@ -1,4 +1,4 @@
-package com.sparta.sogonsogon.chat.Model;
+package com.sparta.sogonsogon.chat.entity;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -7,13 +7,13 @@ import java.util.Date;
 
 @Getter
 @Setter
-public class Message {
+public class Chat {
 
     private String username;
     private String content;
     private Date date;
 
-    public Message(String username, String content, Date date) {
+    public Chat(String username, String content, Date date) {
         this.username = username;
         this.content = content;
         this.date = date;
