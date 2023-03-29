3133bdf88729056087b096fb6090c75a23bfb24b
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-03-29 19:46:43 +0900

    feat: ChattingDto 파일 추가

---
+++ src/main/java/com/sparta/sogonsogon/chat/dto/ChattingDto.java
@@ -0,0 +1,27 @@
+package com.sparta.sogonsogon.chat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChattingDto {
+
+
+    @NotNull(message = "보내는 사람이 없습니다.")
+    private String sender;
+
+    @NotNull(message = "보내는 메세지가 없습니다.")
+    private String message;
+
+    private String sendTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+
+
+}
