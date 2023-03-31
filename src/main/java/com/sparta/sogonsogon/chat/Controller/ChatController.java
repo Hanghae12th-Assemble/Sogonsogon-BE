0ec937e3e62c3178dd53b0103192475b0a5a6477
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-03-31 15:33:35 +0900

    docs: swagger용 더미 api 생성

--- src/main/java/com/sparta/sogonsogon/chat/Controller/ChatController.java
+++ src/main/java/com/sparta/sogonsogon/chat/Controller/ChatController.java
@@ -7,15 +7,15 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -34,15 +34,23 @@ public class ChatController {
     @MessageMapping("/{radioId}")
     @SendTo("/chat/{radioId}")
     @ApiOperation(value = "chatting", notes = "라디오 채팅 기능")
-    public void createChat(@DestinationVariable Long radioId, @Payload String message, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public void createChat(@DestinationVariable Long radioId, @RequestBody String message, @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         ChattingDto chat = ChattingDto.builder()
                 .type(TALK)
-                .sender(userDetails.getUsername())
+                .sender(userDetails.getUser().getNickname())
                 .message(message)
                 .radioId(radioId)
                 .build();
         log.info("CHAT {}", chat);
         template.convertAndSend("/chat/" + radioId, chat);
+    }
+
+    @PostMapping("/{radioId}/send-message")
+    @ApiOperation(value = "chatting", notes = "라디오 채팅 기능 (웹소켓 정보)")
+    public ResponseEntity<?> sendMessage(@PathVariable Long radioId, @RequestBody String message, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+// 이 메서드는 실제로 메시지를 보내지 않으며, Swagger에서 웹소켓 API 정보를 표시하기 위한 목적으로만 사용됩니다.
+
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("이 API는 웹소켓 API 정보를 위한 것이며, 실제 메시지 전송은 웹소켓을 사용하세요.");
     }
 }
